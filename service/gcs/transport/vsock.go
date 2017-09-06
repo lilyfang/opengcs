6ccaecdd07afa1daf2a6cd7e59344a140fba7f91
Akash Gupta <akagup@microsoft.com>
2017-09-06 14:35:28 -0700

    Add a retry when connecting stdin/stdout/stderr

--- service/gcs/transport/vsock.go
+++ service/gcs/transport/vsock.go
@@ -1,9 +1,13 @@
 package transport
 
 import (
-	"github.com/sirupsen/logrus"
+	"fmt"
+	"time"
+
 	"github.com/linuxkit/virtsock/pkg/vsock"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/sys/unix"
 )
 
 const (
@@ -20,13 +24,21 @@ var _ Transport = &VsockTransport{}
 // Dial accepts a vsock socket port number as configuration, and
 // returns an unconnected VsockConnection struct.
 func (t *VsockTransport) Dial(port uint32) (Connection, error) {
-	logrus.Infof("vsock Dial port (%d)", port)
-
-	conn, err := vsock.Dial(vmaddrCidHost, port)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed connecting the VsockConnection")
+	// HACK: Remove loop when vsock bugs are fixed!
+	// Retry 10 times because vsock.Dial can return connection time out
+	// due to some underlying kernel bug.
+	for i := 0; i < 10; i++ {
+		logrus.Infof("vsock Dial port (%d)", port)
+		conn, err := vsock.Dial(vmaddrCidHost, port)
+		if err == nil {
+			logrus.Infof("vsock Connect port (%d)", port)
+			return conn, nil
+		}
+		if err != unix.ETIMEDOUT {
+			return nil, errors.Wrap(err, "failed connecting the VsockConnection")
+		}
+		// Otherwise, we get a bogus timeout error, so just sleep and try again
+		time.Sleep(100 * time.Millisecond)
 	}
-	logrus.Infof("vsock Connect port (%d)", port)
-
-	return conn, nil
+	return nil, fmt.Errorf("failed connecting the VsockConnection: can't connect after 10 attempts")
 }
